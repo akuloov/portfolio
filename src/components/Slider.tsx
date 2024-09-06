f308cf08682685925c6a75955c3f911aa4e13c95
akuloov <maxim.akulovka@gmail.com>
2024-09-06 11:11:32 +0200

    fixing swiper on deployed version

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -55,21 +55,21 @@ export default function Slider(
         className={cn("mySwiper max-w-[305px]", className)}
         pagination={{clickable: true}}
       >
-        <SwiperSlide className={cn("flex flex-col", variants({themeColor}))}>
+        <SwiperSlide className={cn(variants({themeColor}))}>
           <a href="https://react.dev/" target="_blank" className="flex gap-2 mb-4 text-xl font-bold">
             <h1>React</h1>
             <Image src={link} alt="link"/>
           </a>
           <Image src={react} alt="react" width={200}/>
         </SwiperSlide>
-        <SwiperSlide className={cn("flex flex-col", variants({themeColor}))}>
+        <SwiperSlide className={cn(variants({themeColor}))}>
           <a href="https://nextjs.org/" target="_blank" className="flex gap-2 mb-4 text-xl font-bold">
             <h1>Next JS</h1>
             <Image src={link} alt="link"/>
           </a>
           <Image src={next} alt="nextjs" width={200}/>
         </SwiperSlide>
-        <SwiperSlide className={cn("flex flex-col", variants({themeColor}))}>
+        <SwiperSlide className={cn(variants({themeColor}))}>
           <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"
              className="flex gap-2 mb-4 text-xl font-bold">
             <h1>Javascript</h1>
@@ -77,7 +77,7 @@ export default function Slider(
           </a>
           <Image src={js} alt="javascript" width={200}/>
         </SwiperSlide>
-        <SwiperSlide className={cn("flex flex-col", variants({themeColor}))}>
+        <SwiperSlide className={cn(variants({themeColor}))}>
           <a href="https://www.typescriptlang.org/" target="_blank"
              className="flex gap-2 mb-4 text-xl font-bold">
             <h1>Typescript</h1>
@@ -85,7 +85,7 @@ export default function Slider(
           </a>
           <Image src={ts} alt="typescript" width={200}/>
         </SwiperSlide>
-        <SwiperSlide className={cn("flex flex-col", variants({themeColor}))}>
+        <SwiperSlide className={cn(variants({themeColor}))}>
           <a href="https://tailwindcss.com/" target="_blank"
              className="flex gap-2 mb-4 text-xl font-bold">
             <h1>Tailwind css</h1>
