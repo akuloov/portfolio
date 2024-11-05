ae882e745b9d95a334155820df633f5e42e96028
akuloov <maxim.akulovka@gmail.com>
2024-11-05 10:22:48 +0100

    added button for routing to admin dashboard and back

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,7 @@ import {auth} from "@/firebase/firebase.config"; // Adjust path if needed
 import {signInWithEmailAndPassword} from "firebase/auth";
 import {useRouter} from 'next/navigation'
 import {Button, TextField} from "@mui/material";
+import LinkButton from "@/components/LinkButton";
 
 const Login: React.FC = ({onLoginSuccess}: { onLoginSuccess?: () => void }) => {
   const [email, setEmail] = useState<string>("");
@@ -25,37 +26,40 @@ const Login: React.FC = ({onLoginSuccess}: { onLoginSuccess?: () => void }) => {
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <div className="bg-white w-64 p-6 mt-6 m-auto rounded flex flex-col gap-6">
-        <TextField
-          id="email"
-          label="Email"
-          type="email"
-          variant="outlined"
-          className="w-full"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <TextField
-          id="password"
-          label="Password"
-          type="password"
-          variant="outlined"
-          className="w-full"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <Button
-          variant="contained"
-          type="submit"
-        >Log in</Button>
-        {error && <p className="text-red-600">{error}</p>}
-      </div>
-    </form>
-  )
-    ;
+    <main
+      className="gap-2 sm:gap-2 md:gap-3 lg:gap-4 flex flex-col text-white m-auto p-2 max-w-xl overflow-hidden relative w-full transition-all sm:p-4 md:p-6 md:mt-4">
+      <LinkButton route={"/"} className=""/>
+      <form onSubmit={handleLogin}>
+        <div className="bg-white w-64 p-6 mt-6 m-auto rounded flex flex-col gap-6">
+          <TextField
+            id="email"
+            label="Email"
+            type="email"
+            variant="outlined"
+            className="w-full"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            variant="outlined"
+            className="w-full"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <Button
+            variant="contained"
+            type="submit"
+          >Log in</Button>
+          {error && <p className="text-red-600">{error}</p>}
+        </div>
+      </form>
+    </main>
+  );
 };
 
 export default Login;
